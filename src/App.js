aca2da68255102ec06fbed7aa3ac48f0c4156229
turbolukoshko <turbo.lukoshko@gmail.com>
2021-02-14 23:49:02 +0200

    created drag and drop uploading files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [drag, setDrag] = useState(false);
+
+  const dragStart = e => {
+    e.preventDefault();
+    setDrag(true);
+  }
+
+  const dragLeave = e => {
+    e.preventDefault();
+    setDrag(false);
+  }
+
+  const drop = e => {
+    e.preventDefault();
+    let files = [...e.dataTransfer.files];
+    console.log(files);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      {drag ? 
+        <div 
+          className="drop-area"
+          onDragStart={e => dragStart(e)}
+          onDragLeave={e => dragLeave(e)}
+          onDragOver={e => dragStart(e)}
+          onDrop={e => drop(e)}
         >
-          Learn React
-        </a>
-      </header>
+          Release files to download
+        </div> :
+        <div
+          onDragStart={e => dragStart(e)}
+          onDragLeave={e => dragLeave(e)}
+          onDragOver={e => dragStart(e)}
+        >Drag and drop files to upload them</div>
+      }
     </div>
   );
 }
